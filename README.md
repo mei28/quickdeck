7b6e67f0ff461c5663ee954c6a7ac12ed60987cf
mei <mei28aquarius@gmail.com>
2023-11-21 18:01:58 +0900

    update

---
+++ README.md
@@ -0,0 +1,23 @@
+# QuickDeck
+
+QuickDeck is a simple command-line utility to quickly open SlideShare presentations in your default web browser. It extracts the embed URL from a SlideShare presentation URL and opens it directly, making it a handy tool for faster access to SlideShare content.
+
+## Installation
+
+You can install QuickDeck by cloning the repository and building it with Cargo, Rust's package manager and build system.
+
+```
+cargo install quickdeck
+```
+
+
+## Usage
+To use QuickDeck, simply pass the SlideShare presentation URL as an argument:
+
+
+```bash
+quickdeck https://www.slideshare.net/example/presentation
+```
+
+If successful, the SlideShare presentation will open in your default web browser.
+
--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,18 @@ use reqwest;
 use tokio;
 use webbrowser;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() < 2 {
-        eprintln!("Usage: {} <slideshare-url>", args[0]);
-        std::process::exit(1);
-    }
-    let url = &args[1];
-
-    let html = reqwest::get(url).await?.text().await?;
-
+async fn run_app(url: &str) -> Result<(), String> {
+    let html = reqwest::get(url)
+        .await
+        .map_err(|e| e.to_string())?
+        .text()
+        .await
+        .map_err(|e| e.to_string())?;
     let embed_url = extract_embed_url(&html)?;
-
-    if webbrowser::open(&embed_url).is_ok() {
-        println!("Slide opened in web browser: {}", embed_url);
-    } else {
-        eprintln!("Failed to open web browser.");
+    if webbrowser::open(&embed_url).is_err() {
+        return Err("Failed to open web browser.".to_string());
     }
-
+    println!("Slide opened in web browser: {}", embed_url);
     Ok(())
 }
 
@@ -33,5 +26,53 @@ fn extract_embed_url(html: &str) -> Result<String, String> {
         Ok(cap[0].to_string())
     } else {
         Err("No embed URL found".to_string())
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <slideshare-url>", args[0]);
+        std::process::exit(1);
+    }
+    let url = &args[1];
+    run_app(url).await.map_err(|e| e.into())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_run_app_with_valid_args() {
+        let url = "https://www.slideshare.net/iTooooooooooooT/itolab-how-to-survey-2017";
+        assert!(run_app(url).await.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_run_app_with_invalid_args() {
+        let url = "https://www.slideshare.net/mei28/notty";
+        assert!(run_app(url).await.is_err());
+    }
+
+    #[test]
+    fn test_extract_embed_url_valid() {
+        let html = r#"
+"iframeEmbed":{"url":"https://www.slideshare.net/slideshow/embed_code/key/6ab6KSKYN23bGZ","height":486,"width":597},
+        "#;
+        let result = extract_embed_url(html);
+        assert!(result.is_ok());
+        assert_eq!(
+            result.unwrap(),
+            "https://www.slideshare.net/slideshow/embed_code/key/6ab6KSKYN23bGZ"
+        );
+    }
+
+    #[test]
+    fn test_extract_embed_url_invalid() {
+        let html = "<html></html>";
+        let result = extract_embed_url(html);
+        assert!(result.is_err());
     }
 }
